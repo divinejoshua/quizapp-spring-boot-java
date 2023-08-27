06b3d050b4f9dfd734d2e385468eecc6651629f0
divinejoshua <divineuche399@gmail.com>
2023-08-27 16:10:00 +0100

    Made changes

--- src/main/java/com/divinejoshua/quizapp/QuizappApplication.java
+++ src/main/java/com/divinejoshua/quizapp/QuizappApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuizappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(QuizappApplication.class, args);
 	}
