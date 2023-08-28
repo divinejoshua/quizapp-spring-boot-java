1a1a654ab8c6e1af4af06d696617e6478507feee
divinejoshua <divineuche399@gmail.com>
2023-08-28 17:15:03 +0100

    Added question controller and requesting mapping

---
+++ src/main/java/com/divinejoshua/quizapp/QuestionController.java
@@ -0,0 +1,17 @@
+package com.divinejoshua.quizapp;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")  //Parent routing
+public class QuestionController {
+
+    //Get all questions
+    @GetMapping("allquestions")
+    public String getAllQuestions(){
+        return "These are all the questions";
+    }
+}
