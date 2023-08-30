400444fc9eb6725c1e9737357979c5d6d6fbe481
divinejoshua <divineuche399@gmail.com>
2023-08-30 11:24:07 +0100

    Added autowired to Question service

--- src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
+++ src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
@@ -1,6 +1,7 @@
 package com.divinejoshua.quizapp.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
 //    The question service
+    @Autowired //Auto wired is so I dont need to use the 'New' keyword
     QuestionService questionService;
 
     //Get all questions
