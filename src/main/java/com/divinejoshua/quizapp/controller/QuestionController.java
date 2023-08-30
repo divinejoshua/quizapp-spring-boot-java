02abf6f49a21ffbc8681140f420e7d7c21bc8571
divinejoshua <divineuche399@gmail.com>
2023-08-30 10:15:44 +0100

    Made changes

--- src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
+++ src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
@@ -12,6 +12,6 @@ public class QuestionController {
     //Get all questions
     @GetMapping("allquestions")
     public String getAllQuestions(){
-        return "These are all the questions";
+        return "These are all the questions.";
     }
 }
