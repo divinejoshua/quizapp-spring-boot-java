8823eaf5cae67d7733d7f564598cf0f55cf04654
divinejoshua <divineuche399@gmail.com>
2023-08-31 09:30:02 +0100

    Added get question by id to controller

--- src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
+++ src/main/java/com/divinejoshua/quizapp/controller/QuestionController.java
@@ -3,6 +3,7 @@ import com.divinejoshua.quizapp.model.QuestionModel;
 import com.divinejoshua.quizapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,12 @@ public class QuestionController {
     @GetMapping("allquestions")
     public List<QuestionModel> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+
+
+    //Get Question by Id
+    @GetMapping("question/{id}")
+    public String getQuestionById(@PathVariable Integer id){
+        return "Question by ID";
     }
 }
